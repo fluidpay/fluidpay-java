b463af81e82af05ed31d5965ef111f6078fad34b
brianvoe <brian@webiswhatido.com>
2026-01-13 13:23:37 -0600

    test - added test api

---
+++ src/test/java/com/fluidpay/sdk/TestConstants.java
@@ -0,0 +1,15 @@
+package com.fluidpay.sdk;
+
+/**
+ * Shared constants for all tests
+ */
+public class TestConstants {
+    /**
+     * API key for localhost testing
+     */
+    public static final String TEST_API_KEY = "api_0wUsHIlrkK1I6ADno5MfT10UjhR";
+    
+    private TestConstants() {
+        // Utility class - prevent instantiation
+    }
+}
